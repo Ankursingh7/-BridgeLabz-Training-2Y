96676d49cc015b54e48c8d3110802dae15e4d0eb
Ankursingh7 <ankur.singh_cs.aiml24@gla.ac.in>
2025-08-14 11:53:35 +0530

    Java-Fundamental

---
+++ public class VolumeOfCylinder {.java
@@ -0,0 +1,8 @@
+public class VolumeOfCylinder {
+    public static void main(String[] args) {
+        int radius = 3;
+        int height = 10;
+        int volume = 3 * radius * radius * height;  // π approximated as 3
+        System.out.println(volume);
+    }
+}
