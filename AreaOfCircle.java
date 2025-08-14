07bd7fa172430ef421e311faff23088bf8ab1902
Ankursingh7 <ankur.singh_cs.aiml24@gla.ac.in>
2025-08-14 11:15:20 +0530

    Java-Fundamental

---
+++ AreaOfCircle.java
@@ -0,0 +1,7 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        int radius = 5;  // hardcoded radius
+        int area = 3 * radius * radius;  // π approximated as 3
+        System.out.println(area);
+    }
+}
