8c02148ac19b642daaa870926f46f3b479f2212b
Ankursingh7 <ankur.singh_cs.aiml24@gla.ac.in>
2025-08-14 11:13:56 +0530

    Java-Fundamental

---
+++ CelsiusToFahrenheit.java
@@ -0,0 +1,7 @@
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        int celsius = 25;  // hardcoded input value
+        int fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.println(fahrenheit);
+    }
+}
