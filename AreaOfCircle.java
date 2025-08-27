cabc85a7dbfec12516b10ad29db8e34af633e7f5
Ankursingh7 <ankur.singh_cs.aiml24@gla.ac.in>
2025-08-27 23:02:13 +0530

    Delete AreaOfCircle.java

--- AreaOfCircle.java
+++
@@ -1,7 +0,0 @@
-public class AreaOfCircle {
-    public static void main(String[] args) {
-        int radius = 5; 
-        int area = 3 * radius * radius; 
-        System.out.println(area);
-    }
-}
