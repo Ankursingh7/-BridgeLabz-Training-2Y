85c9bb37980ee0f1967d1c72a682fc08610e9672
Ankursingh7 <ankur.singh_cs.aiml24@gla.ac.in>
2025-08-27 23:03:38 +0530

    Delete CelsiusToFahrenheit.java

--- CelsiusToFahrenheit.java
+++
@@ -1,7 +0,0 @@
-public class CelsiusToFahrenheit {
-    public static void main(String[] args) {
-        int celsius = 25; 
-        int fahrenheit = (celsius * 9 / 5) + 32;
-        System.out.println(fahrenheit);
-    }
-}
